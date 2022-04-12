127dfd43f49cf180014ebd087eeef0a5df8bfd97
NevenaDarijevic <darijevicnevena00@gmail.com>
2022-04-12 14:33:36 +0200

    First migration executed (initial)

---
+++ Product/Migrations/20220412122416_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Product.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        //this i migration file, steps performed on database when I do update
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proizvod",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cena = table.Column<double>(type: "float", nullable: false),
+                    Pdv = table.Column<double>(type: "float", nullable: false),
+                    TipProizvodaId = table.Column<long>(type: "bigint", nullable: false),
+                    JedinicaMereId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proizvod", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Proizvod");
+        }
+    }
+}
