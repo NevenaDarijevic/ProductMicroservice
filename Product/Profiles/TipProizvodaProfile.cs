b0aafc0c06a40379cb1ac4876bc6805a0260c77b
NevenaDarijevic <darijevicnevena00@gmail.com>
2022-04-12 16:35:04 +0200

    Created all mappers

---
+++ Product/Profiles/TipProizvodaProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Product.DTOs;
+using Product.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Profiles
+{
+    public class TipProizvodaProfile : Profile
+    {
+        public TipProizvodaProfile()
+        {
+            CreateMap<TipProizvoda, TipProizvodaDTO>();
+            CreateMap<TipProizvodaDTO, TipProizvoda>();
+        }
+    }
+}
